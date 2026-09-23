806a36f98b0b80c994fd1071d696710292784f5b
nelbio <167593673+nelbio@users.noreply.github.com>
2026-09-23 11:00:22 +0100

    fist commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# $env:Path += ";$env:USERPROFILE\.local\bin" 
+ Exécutez cette commande dans la fenêtre PowerShell où l'erreur s'est produite pour ajouter immédiatement le dossier à votre session :
---
+++ src/learningfast/app.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
+from src.learningfast.schemas import PostCreate, PostResponse
+from src.learningfast.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy import select
+from src.learningfast.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import shutil
+import os
+import uuid
+import tempfile
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+     temp_file_path = None
+
+     try:
+        with tempfile.NamedTemporaryfile(delete=False, suffix=os.path.splittext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend-upload"]
+            )
+        ) 
+        if upload_result.response.http_status_code == 200:
+            post = Post(
+                 caption=caption,
+                url=upload_result.url,
+                file_type="video" if file.content_type.startswith("video/") else "image",
+             file_name=upload_result.name
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+     except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        pass
+     finally:
+            if temp_file_path and os.path.exists(temp_file_path):
+                os.unlink(temp_file_path)
+                file.file.close()
+
+
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+           { 
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat() 
+            }
+        )
+    return {"posts": posts_data}   
